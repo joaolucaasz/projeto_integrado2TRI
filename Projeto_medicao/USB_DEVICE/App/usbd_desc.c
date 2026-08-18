eeb17448d43b6615555ac6dc8e06e08e5341a8f1
IsabelaPivoto <pivotoisabela@email.com.br>
2026-08-18 09:09:05 -0300

    usb 12345678

--- Projeto_medicao/USB_DEVICE/App/usbd_desc.c
+++ Projeto_medicao/USB_DEVICE/App/usbd_desc.c
@@ -339,17 +339,18 @@ static void Get_SerialNum(void)
 {
   uint32_t deviceserial0, deviceserial1, deviceserial2;
 
-  deviceserial0 = *(uint32_t *) DEVICE_ID1;
-  deviceserial1 = *(uint32_t *) DEVICE_ID2;
-  deviceserial2 = *(uint32_t *) DEVICE_ID3;
+  //deviceserial0 = *(uint32_t *) DEVICE_ID1;
+ // deviceserial1 = *(uint32_t *) DEVICE_ID2;
+ // deviceserial2 = *(uint32_t *) DEVICE_ID3;
 
-  deviceserial0 += deviceserial2;
+  //deviceserial0 += deviceserial2;
 
-  if (deviceserial0 != 0)
-  {
-    IntToUnicode(deviceserial0, &USBD_StringSerial[2], 8);
-    IntToUnicode(deviceserial1, &USBD_StringSerial[18], 4);
-  }
+  //if (deviceserial0 != 0)
+ // {
+ //   IntToUnicode(deviceserial0, &USBD_StringSerial[2], 8);
+  //  IntToUnicode(deviceserial1, &USBD_StringSerial[18], 4);
+ // }
+  strcpy (USBD_StringSerial,"12345678");
 }
 
 /**
